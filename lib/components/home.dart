db3fbd6d55315830d37540c38261a4c19a6e18ba
mohankumargupta <mohankumargupta@gmail.com>
2022-09-15 10:10:23 +1000

    added top and bottom appbars

--- lib/components/home.dart
+++ lib/components/home.dart
@@ -7,38 +7,66 @@ class MyHomePage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Material(
-        child: Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
+        child: Column(
       children: [
-        NavigationRail(
-            backgroundColor: Colors.pinkAccent,
-            //labelType: NavigationRailLabelType.all,
-            extended: true,
-            //minExtendedWidth: 300.0,
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: const [
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 0.0),
+              child: Text("LHS"),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 0.0),
+              child: Text("RHS"),
+            ),
+          ],
+        ),
+        Expanded(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              NavigationRail(
+                  backgroundColor: Colors.pinkAccent,
+                  //labelType: NavigationRailLabelType.all,
+                  extended: true,
+                  //minExtendedWidth: 300.0,
 
-            //minWidth: 300.0,
-            selectedIconTheme: const IconThemeData(size: 80),
-            unselectedIconTheme: const IconThemeData(size: 80),
-            destinations: const [
-              NavigationRailDestination(
-                  padding: EdgeInsets.only(
-                      left: 50.0, right: 50.0, bottom: 20.0, top: 100.0),
-                  icon: Icon(Icons.breakfast_dining),
-                  label: Padding(
-                    padding: EdgeInsets.only(left: 50.0, right: 50.0),
-                    child: Text("Area"),
-                  )),
-              NavigationRailDestination(
-                  padding: EdgeInsets.only(
-                      left: 50.0, right: 50.0, bottom: 20.0, top: 20.0),
-                  icon: Icon(Icons.devices_other),
-                  label: Padding(
-                    padding: EdgeInsets.only(left: 50.0, right: 50.0),
-                    child: Text("Device"),
-                  )),
+                  //minWidth: 300.0,
+                  selectedIconTheme: const IconThemeData(size: 80),
+                  unselectedIconTheme: const IconThemeData(size: 80),
+                  destinations: const [
+                    NavigationRailDestination(
+                        padding: EdgeInsets.only(
+                            left: 50.0, right: 50.0, bottom: 20.0, top: 100.0),
+                        icon: Icon(Icons.breakfast_dining),
+                        label: Padding(
+                          padding: EdgeInsets.only(left: 50.0, right: 50.0),
+                          child: Text("Area"),
+                        )),
+                    NavigationRailDestination(
+                        padding: EdgeInsets.only(
+                            left: 50.0, right: 50.0, bottom: 20.0, top: 20.0),
+                        icon: Icon(Icons.devices_other),
+                        label: Padding(
+                          padding: EdgeInsets.only(left: 50.0, right: 50.0),
+                          child: Text("Device"),
+                        )),
+                  ],
+                  selectedIndex: 0),
+              const Expanded(child: Text("RHSdasdadasdasdadda"))
             ],
-            selectedIndex: 0),
-        const Expanded(child: Text("RHSdasdadasdasdadda"))
+          ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: const [
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 0.0),
+              child: Text("copyight 2022 Mohan Gupta"),
+            ),
+          ],
+        ),
       ],
     ));
   }
